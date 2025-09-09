b5e735366b89acd681d802b434a1e2b7802df3a4
Doncolyonjr <dacox6@asu.edu>
2025-09-09 14:32:42 -0700

    update script

--- scripts.js
+++ scripts.js
@@ -15,32 +15,49 @@ function concatenateStrings(str1, str2){
 
 // for the first function expression, let's write code that displays returns one result if the number entered by the user is less than 100 ("number is less than 100"), and another message if the number is greater than or equal to 100 ("that is 100 or greater")
 // name this variable compareToHundred
-// TO DO
+let compareToHundred = function(number){
+if(number < 100){
+    return number + " is less than 100";
+}else
+{ return number + " is greater than or equal to 100";
+
+}
+};
 
 
 // for the second function expression, let's write code that tells the user whether or not their answer to a trivia question is correct. This will accept a parameter for a letter answer, and can use either a switch statement or a series of if/else/if statements for the logic
 // name this variable quizAnswer
-// TO DO
+let quizAnswer = function(answer){
+switch(answer){
 		// if they answered a return "Nope. GIT215 is the Introductory HTML/CSS course."
-
+case "a":
+    return "Nope. GIT215 is the Introductory HTML/CSS course.";
 	
 		// if they answered b return "Nope. GIT337 is the Intermediate HTML/CSS course."
-
+case "b":
+    return "Nope. GIT337 is the Introductory HTML/CSS course.";
 	
 		// if they answered c return "Nope. GIT414 is the Responsive HTML/CSS course."
-
+case "c":
+    return "Nope. GIT414 is the Introductory HTML/CSS course.";
 	
 		// if they answered d return "You got it! GIT417 is the Introductory JavaScript course."
-
+case "d":
+    return "You got it! GIT417 is the Introductory JavaScript course.";
+    }
+    };
 
 // For box five, re-write the first standard function in the arrow function syntax
 // name the variable addTwoNumsArrow
-// TO DO
+let addTwoNumsArrow = (num1, num2) => num1 + num2;
 
 
 // For box five, re-write the second standard function in the arrow function syntax
 // name the variable concatenateStringsArrow
-// TO DO
+let concatenateStringsArrow = (str1, str2) => str1 + " " + str2;
+
+console.log('call addTwoNums(2,2));
+    console.log('concatenateStrings (taco, cat)) ', concatenateStrings("taco", "cat"));
 
 
 
